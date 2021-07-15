8a53e33b7246f193ef3bfc267515eb095daa9457
Balabukh Nick <nick_bron@ukr.net>
2021-07-15 15:35:17 +0300

    create fetchCountries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import './css/styles.css';
-
+import fetchCountries from "./js/fetchCountries"
 const DEBOUNCE_DELAY = 300;
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,3 @@
+export default function fetchCountries (e){
+    e.preventDefault();
+}
