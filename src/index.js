d66074e879e4f9f744b835dbfc1a8c6d885cfb4a
Balabukh Nick <nick_bron@ukr.net>
2021-07-16 14:59:14 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
-import fetchCountries from "./js/fetchCountries"
+import fetchCountries from "./js/fetchCountries";
+import getRefs from './js/get-refs';
+//import API from './api-service';
 const DEBOUNCE_DELAY = 300;
+
+const refs = getRefs();
+
+refs.searchForm.addEventListener('input', onSearch);
+ 
+function onSearch(e){
+    e.preventDefault();
+    const form = e.currentTarget;
+  //const searchQuery = form.elements.query.value;
+  console.log(form)
+  //console.log(searchQuery)
+}
